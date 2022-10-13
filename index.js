c7231ad8ec8d18643228e4aee9a8ddbd861d053a
Yousif A. Dhahir <Yusuf.it.01@gmail.com>
2022-10-13 14:27:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 25;
+const num4 = 7;
+
+const mod = num3 % num4;
+
+let max = Math.max(10, 20, 14, 2, 5, 2, 8);
